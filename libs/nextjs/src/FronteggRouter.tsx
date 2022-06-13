0165e317d7cfbb8837fbffe2dc2f0a6c3a948a69
David Antoon <david@frontegg.com>
2022-06-13 22:23:25 +0300

    FR-5604 - finalize SSR rendering with hosted login and SSR session

--- libs/nextjs/src/FronteggRouter.tsx
+++ libs/nextjs/src/FronteggRouter.tsx
@@ -1,8 +1,34 @@
 import FronteggConfig from './FronteggConfig';
 import { authInitialState } from '@frontegg/redux-store';
 import { parse } from 'url';
+import { useContext, useEffect } from 'react';
+import AppContext from './AppContext';
+import { useRouter } from 'next/router';
+import { useLoginWithRedirect } from '@frontegg/react-hooks';
 
 export function FronteggRouter() {
+  const app = useContext(AppContext);
+  const { query } = useRouter();
+  const loginWithRedirect = useLoginWithRedirect()
+  useEffect(() => {
+    if (!app) {
+      return;
+    }
+    if (app.options.hostedLoginBox) {
+      const routesObj = {
+        ...authInitialState.routes,
+        ...FronteggConfig.authRoutes,
+      };
+
+      const { 'frontegg-router': pathArr, ...queryParams } = query as any;
+      const pathname = `/${pathArr.join('/')}`;
+      if (pathname === routesObj.loginUrl) {
+        loginWithRedirect()
+      } else {
+        console.log('NO', pathname, queryParams)
+      }
+    }
+  }, [ app, query, loginWithRedirect ])
   return '';
 }
 
@@ -12,7 +38,7 @@ export function FronteggRouterProps(context: any) {
     ...FronteggConfig.authRoutes,
   };
   const routesArr: string[] = Object.keys(routesObj).reduce(
-    (p: string[], key: string) => [...p, (routesObj as any)[key]],
+    (p: string[], key: string) => [ ...p, (routesObj as any)[key] ],
     []
   );
 
@@ -22,6 +48,11 @@ export function FronteggRouterProps(context: any) {
     pathname = `/${Array.isArray(query) ? query.join('/') : query}`;
   }
   const notFound = routesArr.indexOf(pathname as string) === -1;
+
+  if (FronteggConfig.fronteggAppOptions.hostedLoginBox) {
+    const notFound = !(routesObj.loginUrl === pathname || routesObj.hostedLoginRedirectUrl === pathname);
+    return { notFound, props: {} }
+  }
   return {
     notFound,
     props: {},
