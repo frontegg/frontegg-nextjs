34718fba03c68ee91f142ccd9c2634b411a50901
Rotem <rotem@frontegg.com>
2022-11-15 17:26:27 +0200

    FR-9574 - expose rest api

--- libs/nextjs/src/index.ts
+++ libs/nextjs/src/index.ts
@@ -10,3 +10,4 @@ export * from './FronteggMiddleware';
 export { AdminPortal } from '@frontegg/js';
 export * from '@frontegg/react-hooks';
 export * from '@frontegg/types';
+export * from '@frontegg/rest-api';
