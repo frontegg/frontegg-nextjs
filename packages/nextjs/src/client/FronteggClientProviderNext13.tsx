a06c69208a4b8142f3005dfd98ec8fddbf1f7741
Yuval Lotem <yuval.lotem@frontegg.com>
2022-12-18 12:48:32 +0200

    update version and access next config basePath

--- packages/nextjs/src/client/FronteggClientProviderNext13.tsx
+++ packages/nextjs/src/client/FronteggClientProviderNext13.tsx
@@ -3,11 +3,16 @@ import { FronteggBaseProvider } from '../common-client';
 import { useRouter } from 'next/navigation';
 import React, { FC } from 'react';
 
-export const FronteggClientProviderNext13: FC<Omit<FronteggProviderProps, 'router'>> = ({ children, ...props }) => {
+export const FronteggClientProviderNext13: FC<Omit<FronteggProviderProps, 'router'>> = ({
+  children,
+  basename,
+  ...props
+}) => {
   const router = useRouter();
+  const basePath = process.env.__NEXT_ROUTER_BASEPATH || '';
 
   return (
-    <FronteggBaseProvider router={router} {...props}>
+    <FronteggBaseProvider router={router} basename={basename ?? basePath} {...props}>
       {children}
     </FronteggBaseProvider>
   );
