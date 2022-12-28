df5cc469b05c17b6983acfab66ed0d5e114be8ef
Yuval Lotem <yuval.lotem@frontegg.com>
2022-12-28 11:48:39 +0200

    cookie manager imporvements
    decrease cookie limit, remove old cookie name from parseCookieFromArray, enhance getCookieName and tests

--- packages/nextjs/test/src/common/CookieManager.spec.ts
+++ packages/nextjs/test/src/common/CookieManager.spec.ts
@@ -1,10 +1,11 @@
 import { CookieManager, FronteggConfig } from '../../../src';
-import { smallCookieValue, bigCookieValue, cookieDomain, cookieName } from './const';
+import { COOKIE_MAX_LENGTH } from '../../../src/common/consts';
+import { SMALL_COOKIE_VALUE, LARGE_COOKIE_VALUE, COOKIE_DOMAIN, COOKIE_NAME } from './const';
 
 const extractValueOutOfCookie = (cookie: string) =>
   cookie
     .split('; ')
-    ?.find((c) => c.includes(cookieName))
+    ?.find((c) => c.includes(COOKIE_NAME))
     ?.split('=')[1] ?? '';
 
 describe('Cookie Manager', () => {
@@ -16,31 +17,31 @@ describe('Cookie Manager', () => {
 
   it('createCookie should create cookie properly', () => {
     const cookieValue = CookieManager.createCookie({
-      cookieName,
-      value: smallCookieValue,
+      cookieName: COOKIE_NAME,
+      value: SMALL_COOKIE_VALUE,
       expires: new Date(),
       isSecured: true,
-      cookieDomain,
+      cookieDomain: COOKIE_DOMAIN,
     });
 
     expect(cookieValue.length).toEqual(1);
-    expect(cookieValue[0]).toContain(`${cookieName}=${smallCookieValue}`);
+    expect(cookieValue[0]).toContain(`${COOKIE_NAME}=${SMALL_COOKIE_VALUE}`);
     expect(cookieValue[0]).toContain('HttpOnly');
     expect(cookieValue[0]).toContain('Secure');
     expect(cookieValue[0]).toContain('SameSite=None');
-    expect(cookieValue[0]).toContain(`Domain=${cookieDomain}`);
+    expect(cookieValue[0]).toContain(`Domain=${COOKIE_DOMAIN}`);
   });
 
   it('createCookie with big value should create split cookie properly', () => {
     const cookieValue = CookieManager.createCookie({
-      cookieName,
-      value: bigCookieValue,
+      cookieName: COOKIE_NAME,
+      value: LARGE_COOKIE_VALUE,
       expires: new Date(),
       isSecured: true,
-      cookieDomain,
+      cookieDomain: COOKIE_DOMAIN,
     });
-    const firstCookieName = CookieManager.getCookieName(1, cookieName);
-    const secondCookieName = CookieManager.getCookieName(2, cookieName);
+    const firstCookieName = CookieManager.getCookieName(1, COOKIE_NAME);
+    const secondCookieName = CookieManager.getCookieName(2, COOKIE_NAME);
 
     const firstCookieValue = extractValueOutOfCookie(cookieValue[0]);
     const secondCookieValue = extractValueOutOfCookie(cookieValue[1]);
@@ -51,23 +52,25 @@ describe('Cookie Manager', () => {
     expect(cookieValue[0]).toContain('HttpOnly');
     expect(cookieValue[0]).toContain('Secure');
     expect(cookieValue[0]).toContain('SameSite=None');
-    expect(cookieValue[0]).toContain(`Domain=${cookieDomain}`);
+    expect(cookieValue[0]).toContain(`Domain=${COOKIE_DOMAIN}`);
+    expect(cookieValue[0].length).toEqual(COOKIE_MAX_LENGTH);
 
     expect(cookieValue[1]).toContain(`${secondCookieName}=${secondCookieValue}`);
     expect(cookieValue[1]).toContain('HttpOnly');
     expect(cookieValue[1]).toContain('Secure');
     expect(cookieValue[1]).toContain('SameSite=None');
-    expect(cookieValue[1]).toContain(`Domain=${cookieDomain}`);
+    expect(cookieValue[1]).toContain(`Domain=${COOKIE_DOMAIN}`);
+    expect(cookieValue[1].length).toBeLessThan(COOKIE_MAX_LENGTH);
 
-    expect(firstCookieValue + secondCookieValue).toEqual(bigCookieValue);
+    expect(firstCookieValue + secondCookieValue).toEqual(LARGE_COOKIE_VALUE);
   });
 
   it('createEmptyCookies should accepts cookie names and create cookies with empty value and expiration date equals to now', () => {
-    const firstCookieName = CookieManager.getCookieName(1, cookieName);
-    const secondCookieName = CookieManager.getCookieName(2, cookieName);
-    const cookiesToRemove = [cookieName, firstCookieName, secondCookieName];
+    const firstCookieName = CookieManager.getCookieName(1, COOKIE_NAME);
+    const secondCookieName = CookieManager.getCookieName(2, COOKIE_NAME);
+    const cookiesToRemove = [COOKIE_NAME, firstCookieName, secondCookieName];
     const isSecured = true;
-    const emptyCookies = CookieManager.createEmptyCookies(isSecured, cookieDomain, cookiesToRemove);
+    const emptyCookies = CookieManager.createEmptyCookies(isSecured, COOKIE_DOMAIN, cookiesToRemove);
 
     expect(emptyCookies.length).toEqual(cookiesToRemove.length);
     cookiesToRemove.forEach((name, index) => {
@@ -75,7 +78,7 @@ describe('Cookie Manager', () => {
       expect(emptyCookies[index]).toContain('HttpOnly');
       expect(emptyCookies[index]).toContain('Secure');
       expect(emptyCookies[index]).toContain('SameSite=None');
-      expect(emptyCookies[index]).toContain(`Domain=${cookieDomain}`);
+      expect(emptyCookies[index]).toContain(`Domain=${COOKIE_DOMAIN}`);
       expect(emptyCookies[index]).toContain(`Expires=${new Date().toUTCString()}`);
     });
   });
