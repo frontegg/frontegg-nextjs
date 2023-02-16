b52ea37655ea09b1f2dc70398eee20c34df5c6dd
David Antoon <david@frontegg.com>
2023-02-16 22:30:22 +0200

    test danger-js

--- dangerjs/dangerfile.ts
+++ dangerjs/dangerfile.ts
@@ -1,18 +1,69 @@
 // import { markdown, danger, warn, fail, schedule, message } from 'danger';
-import { danger, markdown, message, warn } from 'danger';
-// import checkYarnLock from './check-yarn-lock';
-// import { checkDebugger } from './check-debugger';
-// import { printSummary } from './print-summary';
-// import { checkPackageLock } from './check-package-lock';
-import checkDependencies from './check-dependencies';
-import yarn from 'danger-plugin-yarn';
+import { danger, fail, markdown, message, warn } from 'danger';
+
+// import yarn from 'danger-plugin-yarn';
+
+function printSummary() {
+  const docs = danger.git.fileMatch('**/*.md');
+  const next12App = danger.git.fileMatch('packages/demo-saas/**/*');
+  const next13App = danger.git.fileMatch('packages/demo-saas-next12/**/*');
+  const library = danger.git.fileMatch('packages/nextjs/**/*');
+  const tests = danger.git.fileMatch('*/unit-tests/*');
+
+  const summery = ['### Summary:'];
+  docs.edited && summery.push('- Detect changes in docs.');
+  tests.edited && summery.push('- Detect changes in Unit Tests.');
+  next12App.edited && summery.push('- Detected changes in `Next.js 12` example project.');
+  next13App.edited && summery.push('- Detected changes in `Next.js 13` example project.');
+  library.edited && summery.push('- Detected change in `@frontegg/nextjs`.');
+
+  markdown(summery.join('\n'));
+}
+
+function checkYarnLock() {
+  const packageChanged = global.danger.git.modified_files.includes('package.json');
+  const lockfileChanged = global.git.modified_files.includes('yarn.lock');
+  if (packageChanged && !lockfileChanged) {
+    const message = 'Changes were made to package.json, but not to yarn.lock';
+    const idea = 'Perhaps you need to run `yarn install`?';
+    fail(`${message} - <i>${idea}</i>`);
+  }
+}
+
+function checkDebugger() {}
+
+function checkPackageLock() {
+  const npmLockFiles = danger.git.fileMatch('**/package-lock.json');
+
+  if (npmLockFiles.edited) {
+    fail(
+      `Detected package-lock file. remove all package-lock.json and use \`yarn install\` for installing dependencies`
+    );
+  }
+}
+
+const dependencyCodeOwners = ['frontegg-david'];
+
+function checkDependencies() {
+  // Warns if there are changes to package.json, and tags the team.
+  const packageChanged = danger.git.modified_files.includes('package.json');
+  const reviews = danger.github.reviews;
+
+  reviews.find((review) => review.state === 'APPROVED' && review.user.login);
+  if (packageChanged) {
+    const title = ':lock: package.json';
+    const mentions = dependencyCodeOwners.map((mention) => `@${mention}`).join(', ');
+    const idea = `Changes made to package.json should be reviewed by DependencyCodeOwners. (${mentions}).`;
+    fail(`${title} - <i>${idea}</i>`);
+  }
+}
 
 markdown('## Frontegg Doctor :heart: report:');
 
-// printSummary();
-// checkYarnLock();
-// checkDebugger();
-// checkPackageLock();
+printSummary();
+checkYarnLock();
+checkDebugger();
+checkPackageLock();
 checkDependencies();
 
 // message(`Remove \`ready_for_review\`, \`review_requested\` from  on:pull_request:types`, {
