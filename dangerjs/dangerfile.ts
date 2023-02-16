667c4e6b21e7158192504a79d5a5b5beb7a601fd
David Antoon <david@frontegg.com>
2023-02-16 22:30:36 +0200

    test danger-js

--- dangerjs/dangerfile.ts
+++ dangerjs/dangerfile.ts
@@ -21,8 +21,8 @@ function printSummary() {
 }
 
 function checkYarnLock() {
-  const packageChanged = global.danger.git.modified_files.includes('package.json');
-  const lockfileChanged = global.git.modified_files.includes('yarn.lock');
+  const packageChanged = danger.git.modified_files.includes('package.json');
+  const lockfileChanged = danger.git.modified_files.includes('yarn.lock');
   if (packageChanged && !lockfileChanged) {
     const message = 'Changes were made to package.json, but not to yarn.lock';
     const idea = 'Perhaps you need to run `yarn install`?';
