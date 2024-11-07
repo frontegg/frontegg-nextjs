765c098bd9d8b20c55ea02177e64c1a32258635f
David Antoon <david@frontegg.com>
2024-11-07 18:00:12 +0200

    bypass session middleware for testing urls

--- packages/example-pages/middleware.ts
+++ packages/example-pages/middleware.ts
@@ -1,7 +1,12 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { handleSessionOnEdge } from '@frontegg/nextjs/edge';
 
 export const middleware = async (request: NextRequest) => {
+  // this if for frontegg middleware tests
+  if (process.env['FRONTEGG_TEST_URL']) {
+    return NextResponse.next();
+  }
+
   const { pathname, searchParams } = request.nextUrl;
   const headers = request.headers;
 
