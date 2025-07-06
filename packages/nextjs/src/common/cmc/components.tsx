a608733fd3aebd692f849685b310c4848e398a28
TomerFrontegg <tomer@frontegg.com>
2025-07-06 15:24:17 +0300

    Fix inifine loop on react 19 with embedded components

--- packages/nextjs/src/common/cmc/components.tsx
+++ packages/nextjs/src/common/cmc/components.tsx
@@ -1,25 +1,31 @@
 'use client';
 
-import { ChangePasswordFormProps, EditEmailFormProps, InviteUserDialogProps, ProfilePageProps, UsersTableProps } from '@frontegg/types';
+import {
+  ChangePasswordFormProps,
+  EditEmailFormProps,
+  InviteUserDialogProps,
+  ProfilePageProps,
+  UsersTableProps,
+} from '@frontegg/types';
 import React, { FC } from 'react';
 import { CMCComponent, FronteggCMCComponentProps } from './base';
 
 export const UsersTable: FC<FronteggCMCComponentProps<UsersTableProps>> = (props) => {
-    return <CMCComponent renderComponent="renderUsersTable" {...props} />
-}
+  return <CMCComponent renderComponent='renderUsersTable' {...props} />;
+};
 
 export const InviteUserDialog: FC<FronteggCMCComponentProps<InviteUserDialogProps>> = (props) => {
-    return <CMCComponent renderComponent="renderInviteUserDialog" {...props} />
-}
+  return <CMCComponent renderComponent='renderInviteUserDialog' {...props} />;
+};
 
 export const ChangePasswordForm: FC<FronteggCMCComponentProps<ChangePasswordFormProps>> = (props) => {
-    return <CMCComponent renderComponent="renderChangePasswordForm" {...props} />
-}
+  return <CMCComponent renderComponent='renderChangePasswordForm' {...props} />;
+};
 
 export const ProfilePage: FC<FronteggCMCComponentProps<ProfilePageProps>> = (props) => {
-    return <CMCComponent renderComponent="renderProfilePage" {...props} />
-}
+  return <CMCComponent renderComponent='renderProfilePage' {...props} />;
+};
 
 export const EditEmailForm: FC<FronteggCMCComponentProps<EditEmailFormProps>> = (props) => {
-    return <CMCComponent renderComponent="renderEditEmailForm" {...props} />
-}
+  return <CMCComponent renderComponent='renderEditEmailForm' {...props} />;
+};
